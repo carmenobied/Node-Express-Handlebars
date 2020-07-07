57198b27fcbbc8d2c5e7a8605da892dfd23680ce
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-07-07 00:07:06 -0700

    updated connection for heroku deployment using process.env.JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@
 let mysql = require("mysql");
 
 // const DB_password = process.env.BurgersDB_URL;
+let connection; 
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Nomad7285!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Nomad7285!",
+    database: "burgers_db"
+  });
+}; 
 
 // Make connection.
 connection.connect(err => {
