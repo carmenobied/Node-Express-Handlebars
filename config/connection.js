c97d41903894b8855b2bf6a5b8f8921ca2e23d70
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-07-07 18:04:06 -0700

    refactored code

--- config/connection.js
+++ config/connection.js
@@ -9,21 +9,13 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
-    password: "Nomad7285!",
+    password: "",
     database: "burgers_db"
   });
 }; 
 
 // Make connection.
-connection.connect(err => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
